e6d31cc9448c7c6d2e9fa2c76bd05908888b81a6
Jitendra Kumar <jk.bhu85@gmail.com>
2018-04-21 15:32:50 +0530

    Add navbar in frontend

--- src/frontend/src/app/modules/app-common.module.ts
+++ src/frontend/src/app/modules/app-common.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     TranslateModule
   ],
   exports: [
     CommonModule,
+    RouterModule,
     TranslateModule
   ]
 })
